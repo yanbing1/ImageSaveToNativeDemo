6c6612a00cc070175fe6e045c6206e05725554ab
曹燕兵 <soutsubamehei@caoyanbingdeiMac.local>
2017-08-18 15:14:14 +0800

    Initial Commit

---
+++ ImageSaveToNativeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageSaveToNativeDemo
+//
+//  Created by 曹燕兵 on 2017/8/18.
+//  Copyright © 2017年 曹燕兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageSaveToNativeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageSaveToNativeDemo
+//
+//  Created by 曹燕兵 on 2017/8/18.
+//  Copyright © 2017年 曹燕兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
